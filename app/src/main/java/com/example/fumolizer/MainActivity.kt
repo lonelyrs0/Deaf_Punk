ef52fc673c693633ee1d745af328eef36f38db47
kaipen <thisisnotfakemaybe@gmail.com>
2022-01-19 14:10:11 -0800

    test

--- app/src/main/java/com/example/fumolizer/MainActivity.kt
+++ app/src/main/java/com/example/fumolizer/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.fumolizer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener(){
+            val text = "Hello toast!"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+        }
+
     }
+
+
+
 }
